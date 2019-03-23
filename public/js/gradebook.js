075b37a44f283907c0c89dbb1c0f5e6ecbd453f9
David Krajewski <davidkrajewski@yahoo.com>
2019-03-23 16:42:46 -0600

    Implemented user sign in through cookies, needs styling. also implemented getting user's grades instead of static grade array.

--- public/js/gradebook.js
+++ public/js/gradebook.js
@@ -1,32 +1,101 @@
-$(document).ready(function() {
-    loadGrades();
+$(document).ready(function () {
+    if ((getCookie("username") == "" || getCookie("username") == null) || (getCookie("password") == "" || getCookie("password") == null)) {
+        displayError();
+    } else {
+        loadGrades();
+    }
 });
+
+function displayError() {
+    $('.main').append(`<h4> Uh Oh! Please Sign In To View Your Grades! </h4>
+                        <input type="text" placeholder="username" id="usernameField" />
+                        <input type="password" placeholder="password" id="passwordField" />
+                        <button onclick="checkLogin()" > Submit </button>
+                        `);
+}
+
+function checkLogin() {
+    const username = document.getElementById('usernameField').value;
+    const password = document.getElementById('passwordField').value;
+    if (username == "" || password == "") {
+        alert("ENTER USER AND PASS!");
+        return;
+    }
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            if (JSON.parse(this.responseText)["success"] == "true") {
+                document.cookie = "username=" + username;
+                document.cookie = "password=" + password;
+                location.reload();
+            } else {
+                alert("Incorrect user or pass!")
+            }
+        }
+    };
+    xhttp.open("POST", "login", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.send(JSON.stringify({
+        "username": username,
+        "password": password
+    }));
+}
 
 function loadGrades() {
     // get grades json object from server through POST
     // here's a dummy array
-    const classes = [{
-        "name": "Aerospace Sci and Engin H II (S2)",
-        "grade": "A 96.67",
-        "teacher": "Roberto Carballo"
-    },
-    {
-        "name": "Computer Science A AP (S1,S2)",
-        "grade": "A 96.43",
-        "teacher": "Mr. Jesse de Vera"
-    },
-    {
-        "name": "Digital Electronics H (S1)",
-        "grade": "",
-        "teacher": "Roberto Carballo"
-    }];
-
-    classes.forEach(currentClass => {
-        $('.main')
-            .append(`<div class="class row">
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            const classes = JSON.parse(this.responseText);
+            classes.forEach(currentClass => {
+                $('.main')
+                    .append(`<div class="class row">
                         <h4><b>Class</b> ${currentClass.name || 'N/A'}</h4>
                         <h4><b>Grade</b> ${currentClass.grade || 'N/A'}</h4>
                         <h4><b>Teacher</b> ${currentClass.teacher || 'N/A'}</h4>
                     </div>`);
-    });
+            });
+        }
+    };
+    xhttp.open("POST", "getGrades", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.send(JSON.stringify({
+        "username": "kraj011",
+        "password": "Davidk123456"
+    }));
+
+
+    // const classes = [{
+    //         "name": "Aerospace Sci and Engin H II (S2)",
+    //         "grade": "A 96.67",
+    //         "teacher": "Roberto Carballo"
+    //     },
+    //     {
+    //         "name": "Computer Science A AP (S1,S2)",
+    //         "grade": "A 96.43",
+    //         "teacher": "Mr. Jesse de Vera"
+    //     },
+    //     {
+    //         "name": "Digital Electronics H (S1)",
+    //         "grade": "",
+    //         "teacher": "Roberto Carballo"
+    //     }
+    // ];
+
+    // classes.forEach(currentClass => {
+    //     $('.main')
+    //         .append(`<div class="class row">
+    //                     <h4><b>Class</b> ${currentClass.name || 'N/A'}</h4>
+    //                     <h4><b>Grade</b> ${currentClass.grade || 'N/A'}</h4>
+    //                     <h4><b>Teacher</b> ${currentClass.teacher || 'N/A'}</h4>
+    //                 </div>`);
+    // });
+}
+
+function getCookie(name) {
+    var value = "; " + document.cookie;
+    var parts = value.split("; " + name + "=");
+    if (parts.length == 2) return parts.pop().split(";").shift();
 }
